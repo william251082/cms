7ebd4f1f4294b50f48719328c2675e0524c834d5
William del Rosario <williamdelrosario@yahoo.com>
2018-08-29 18:39:29 +0200

    ManytoMany self-referencing relation(folllowing/followers)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -69,9 +69,29 @@ class User implements UserInterface, Serializable
 	 */
     private $posts;
 
+	/**
+	 * @ORM\ManyToMany(targetEntity="App\Entity\User", mappedBy="following")
+	 */
+    private $followers;
+
+	/**
+	 * @ORM\ManyToMany(targetEntity="App\Entity\User", inversedBy="followers")
+	 * @ORM\JoinTable(name="following",
+	 *     joinColumns={
+	 *          @ORM\JoinColumn(name="user_id", referencedColumnName="id")
+	 *     },
+	 *     inverseJoinColumns={
+	 *          @ORM\JoinColumn(name="following_user_id", referencedColumnName="id")
+	 *     }
+	 * )
+	 */
+    private $following;
+
     public function __construct()
     {
     	$this->posts = new ArrayCollection();
+    	$this->followers = new ArrayCollection();
+    	$this->following = new ArrayCollection();
     }
 
 	public function getId(): ?int
@@ -259,6 +279,38 @@ class User implements UserInterface, Serializable
 	public function getPosts()
 	{
 		return $this->posts;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getFollowers()
+	{
+		return $this->followers;
+	}
+
+	/**
+	 * @param mixed $followers
+	 */
+	public function setFollowers($followers): void
+	{
+		$this->followers = $followers;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getFollowing()
+	{
+		return $this->following;
+	}
+
+	/**
+	 * @param mixed $following
+	 */
+	public function setFollowing($following): void
+	{
+		$this->following = $following;
 	}
 
 
