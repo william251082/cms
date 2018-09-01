e1ab7aedbaca3474d423efedb3cf7780b2cf483e
William del Rosario <williamdelrosario@yahoo.com>
2018-09-01 17:12:55 +0200

    more tests

--- tests/Mailer/MailerTest.php
+++ tests/Mailer/MailerTest.php
@@ -29,10 +29,35 @@ class MailerTest extends TestCase
 			->disableOriginalConstructor()
 			->getMock();
 
+		$swiftMailer
+			->expects($this->once())
+			->method('send')
+			->with($this->callback(function($subject){
+					$messageStr = (string)$subject;
+					dump($messageStr);
+//					return true;
+				return strpos($messageStr, "From: me@domain.com") !== false
+					&& strpos($messageStr, "Content-Type: text/html; charset=utf-8") !== false
+					&& strpos($messageStr, "Subject: Welcome to Post App!") !== false
+					&& strpos($messageStr, "From: me@domain.com") !== false
+					&& strpos($messageStr, "To: john@doe.com") !== false
+					&& strpos($messageStr, "This is a message body") !== false;
+			}));
+
+
 		$twigMock = $this
 			->getMockBuilder(Twig_Environment::class)
 			->disableOriginalConstructor()
 			->getMock();
+		$twigMock
+			->expects($this->once())
+			->method('render')
+			->with('email/registration.html.twig',
+				[
+					'user' => $user
+				]
+			)
+			->willReturn('This is a message body');
 
 
 		$mailer = new Mailer($swiftMailer, $twigMock, 'me@domain.com');
